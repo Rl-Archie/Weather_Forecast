6438984ffabafbbf805812d1cc267af1b25d3c7d
Shabd <tadipartyshabd@gmail.com>
2025-11-27 13:32:33 +0530

    Gui with graph is complete.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import plotly.express as px
+
+
+def get_data(days):
+    dates = ["2022-25-100", "2022-26-10", "2022-27-10"]
+    temperatures = [2, 1, 8]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -10,4 +19,8 @@ option = st.selectbox("Select Data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature(C)"})
+st.plotly_chart(figure)
 
